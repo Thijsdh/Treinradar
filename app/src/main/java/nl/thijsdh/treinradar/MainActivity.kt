703374b02287ffd371dd4e62a53d1b6fb04402cc
Thijs den Hartog <thijs@thijsdh.nl>
2022-12-08 16:35:11 +0100

    Migrate Google Maps => OSM

--- app/src/main/java/nl/thijsdh/treinradar/MainActivity.kt
+++ app/src/main/java/nl/thijsdh/treinradar/MainActivity.kt
@@ -9,46 +9,21 @@ import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.wear.ambient.AmbientModeSupport
-import androidx.wear.widget.SwipeDismissFrameLayout
+import androidx.preference.PreferenceManager
 import com.google.android.gms.location.*
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.config.Configuration.*
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback,
-    AmbientModeSupport.AmbientCallbackProvider {
-
-    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var map: MapView
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
 
     public override fun onCreate(savedState: Bundle?) {
         super.onCreate(savedState)
-
-        // Set the layout. It only contains a SupportMapFragment and a DismissOverlay.
-        setContentView(R.layout.activity_main)
-
-        // Enable ambient support, so the map remains visible in simplified, low-color display
-        // when the user is no longer actively using the app but the app is still visible on the
-        // watch face.
-        val controller = AmbientModeSupport.attach(this)
-        Log.d(MainActivity::class.java.simpleName, "Is ambient enabled: " + controller.isAmbient)
-
-        // Retrieve the containers for the root of the layout and the map. Margins will need to be
-        // set on them to account for the system window insets.
-        val mapFrameLayout = findViewById<SwipeDismissFrameLayout>(R.id.map_container)
-        mapFrameLayout.addCallback(object : SwipeDismissFrameLayout.Callback() {
-            override fun onDismissed(layout: SwipeDismissFrameLayout) {
-                onBackPressed()
-            }
-        })
-
-        // Obtain the MapFragment and set the async listener to be notified when the map is ready.
-        mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
         // Request location permissions.
         requestLocationPermission()
@@ -62,35 +37,54 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                 // Do work with the new location. In this case, we just log it.
                 Log.d(MainActivity::class.java.simpleName, locationResult.lastLocation.toString())
 
-                if (locationResult.lastLocation == null) return;
-
+                val lastLocation = locationResult.lastLocation ?: return
                 // Center the map on the new location.
-                mapFragment.getMapAsync { map ->
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                        LatLng(
-                            locationResult.lastLocation!!.latitude,
-                            locationResult.lastLocation!!.longitude), 15f))
-                }
+                map.controller.animateTo(org.osmdroid.util.GeoPoint(lastLocation.latitude, lastLocation.longitude))
             }
         }
 
         // Request location updates.
-        startLocationUpdates()
+
+        setContentView(R.layout.activity_main)
+
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        map = findViewById(R.id.map)
+        map.controller.setZoom(15.0)
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
     }
 
     private fun requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already granted. Start the process of requesting location updates.
+            return startLocationUpdates()
+        }
+
         val locationPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { permissions ->
             when {
                 permissions.getOrDefault(ACCESS_FINE_LOCATION, false) -> {
                     // Precise location access granted.
+                    startLocationUpdates()
                 }
                 permissions.getOrDefault(ACCESS_COARSE_LOCATION, false) -> {
                     // Only approximate location access granted.
+                    requestLocationPermission()
                 } else -> {
-                // No location access granted.
-            }
+                    // No location access granted.
+                }
             }
         }
 
@@ -110,14 +104,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
                 ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+            return requestLocationPermission()
         }
         fusedLocationClient.requestLocationUpdates(
             createLocationRequest(),
@@ -125,31 +112,4 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
             Looper.getMainLooper()
         )
     }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-    }
-
-    override fun getAmbientCallback(): AmbientModeSupport.AmbientCallback {
-        return object : AmbientModeSupport.AmbientCallback() {
-            /**
-             * Starts ambient mode on the map.
-             * The API swaps to a non-interactive and low-color rendering of the map when the user is no
-             * longer actively using the app.
-             */
-            override fun onEnterAmbient(ambientDetails: Bundle) {
-                super.onEnterAmbient(ambientDetails)
-                mapFragment.onEnterAmbient(ambientDetails)
-            }
-
-            /**
-             * Exits ambient mode on the map.
-             * The API swaps to the normal rendering of the map when the user starts actively using the app.
-             */
-            override fun onExitAmbient() {
-                super.onExitAmbient()
-                mapFragment.onExitAmbient()
-            }
-        }
-    }
-
 }
